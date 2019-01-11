8ee6a5627ffbc5eab4fa5dcdb762b7046fe0111f
robertsonandrew2019 <42614650+robertsonandrew2019@users.noreply.github.com>
2019-01-11 14:28:25 -0800

    Update Main.java

--- Palindrome Tester/Main.java
+++ Palindrome Tester/Main.java
@@ -1,16 +1,14 @@
 public class Main {
-
+    
     public static void main(String[] args) {
-        // write your code here
         String[] testSuite = {"Palindrome", "Civic", "Radar", "Level", "Eva, can I stab bats in a cave?", "A man, a plan, a canal, Panama", "Mr. Owl ate my metal worm", "Never odd or even", "Doc, Note: I Dissent. A fast never prevents a fatness. I diet on cod."};
         for (int i = 0; i < testSuite.length; i++) {
             boolean check = tester(testSuite[i]);
             if (check) System.out.printf("\n\"%s\" is a valid palindrome.", testSuite[i]);
-            if (!check) System.out.printf("\n\"%s\" is not a valid palindrome.", testSuite[i]);
+            else System.out.printf("\n\"%s\" is not a valid palindrome.", testSuite[i]);
             System.out.println();
         }
     }
-
     public static boolean tester(String pal) {
         pal = removal(pal);
         String lap = "";
@@ -20,9 +18,7 @@ public class Main {
         }
         return pal.equalsIgnoreCase(lap);
     }
-
     public static String removal(String pal) {
-        int len = pal.length() - 1;
         String newPal = "";
         for (int i = 0; i < pal.length(); i++) {
             if ((pal.charAt(i) == ' ') || (pal.charAt(i) == '.') ||(pal.charAt(i) == ',') || (pal.charAt(i) == '?') || (pal.charAt(i) == '!') || (pal.charAt(i) == ':') || (pal.charAt(i) == ';')) {
@@ -32,5 +28,4 @@ public class Main {
         }
         return newPal;
     }
-
 }
